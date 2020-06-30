978d3f9c95cc661b0562c3606c40ddcb61ee6ba6
Joako <joaquinconsoni@gmail.com>
2020-06-30 11:06:15 -0300

    adding to dictionary key ids

--- commons/constants.go
+++ commons/constants.go
@@ -5,9 +5,14 @@ const Height = 15
 
 var Directions = map[rune]int{
 	'w' : 1, // UP
+	32 : 1, // UP
 	'a' : 2, // LEFT
+	10 : 2, // LEFT
 	's' : 3, // DOWN
+	28 : 3, // DOWN
 	'd' : 4, // RIGHT
+	13 : 4, // RIGHT
+
 }
 
 const TickInterval = 500
